b8f19293de04043ffabb7ba158830a17f65d7b0c
ismaelFiguera1 <ismaelfiguera112@gmail.com>
2025-06-05 11:56:59 +0200

    ex3

--- Exercicis3/script.js
+++ Exercicis3/script.js
@@ -14,7 +14,38 @@ console.log(numRand);
 function capIcua(numero) {
   let text = numero.toString();
   text = text.split("");
-  console.log(text);
+  console.log(text.length);
+
+  if (text.length%2==0) {
+    let meitat = (text.length/2)-1;
+    let meitat2 = (text.length/2);
+    while (meitat2<text.length) {
+      if (text[meitat2] == text[meitat]) {
+        meitat2++;
+        meitat--;
+      } else {
+        console.log(`NO es CAPICUA ${numero}`);
+        return;
+      }
+    }
+    console.log(`Èl numero ${numero} ES CAPICUA`);
+  } else {
+    console.log("NO divisible per 2");
+  }
 }
 
-capIcua(5369841);
+capIcua(253693396352);
+
+// Es Primer
+
+function Primer(numero) {
+  if (numero%2==0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+let numero = Primer(25869);
+
+console.log(numero);
